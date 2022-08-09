3e54cc6fa08ee57a3f5d8b7d7fcdbb88c4b5e5e1
antonockr2 <110889343+antonockr2@users.noreply.github.com>
2022-08-09 16:00:49 +0700

    Update contact.php
    update email

--- forms/contact.php
+++ forms/contact.php
@@ -1,6 +1,6 @@
 <?php
 
-$receiving_email_address = 'adebudiono7@gmail.com';
+$receiving_email_address = 'antonockr1@gmail.com';
 
 if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
 include( $php_email_form );
